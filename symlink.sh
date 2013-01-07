f5cfac37f2d6309493b6292e76c427060d2c9ee6
Guilherme W. O. Pereira <guilhermewop@gmail.com>
2013-01-07 15:39:14 -0200

    Script que cria os links simbólicos

---
+++ symlink.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+dir=~/dotfiles # dotfiles directory
+olddir=~/dotfiles_old # old dotfiles backup directory
+files="bashrc vimrc vim zshrc oh-my-zsh"
+
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+for file in $files; do
+echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
